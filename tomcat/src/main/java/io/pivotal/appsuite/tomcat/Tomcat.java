5a779abd899f78b98beed382fbae33d018db0ff1
Chris Cheetham <chris@cheetham.com>
2017-05-25 08:04:07 -0400

    Tidy code

--- tomcat/src/main/java/io/pivotal/appsuite/tomcat/Tomcat.java
+++ tomcat/src/main/java/io/pivotal/appsuite/tomcat/Tomcat.java
@@ -43,7 +43,7 @@ public class Tomcat {
      *
      * @param instance {@code TomcatAdapter} instance
      */
-    public static synchronized void  setAdapterInstance(TomcatAdapter instance) {
+    public static synchronized void setAdapterInstance(TomcatAdapter instance) {
         adapter = instance;
     }
 
